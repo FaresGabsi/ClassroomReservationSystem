6cffc51c4a634162dc650d756845806db4533484
FaresGabsi <gabsifares@issatso.u-sousse.tn>
2024-03-28 14:39:39 +0100

    initial Commit

--- reservation-service/src/main/java/com/ClassroomReservationSystem/reservationservice/Feign/ClassroomClient.java
+++ reservation-service/src/main/java/com/ClassroomReservationSystem/reservationservice/Feign/ClassroomClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "CLASSROOM-SERVICE", url = "http://localhost:8081")
+@FeignClient(name = "CLASSROOM-SERVICE", url = "http://localhost:8090")
 public interface ClassroomClient {
     @GetMapping("classroom/{id}")
     public Classroom getClassroomById(@PathVariable("id") String id);
